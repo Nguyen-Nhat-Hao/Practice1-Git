b8870e4b5f9fa335c012cb6ea8bfba666bb79b87
Nguyen Nhat Hao <nguyennhathao20022018@gmail.com>
2023-05-20 13:41:14 +0700

    push code

--- src/components/TestDeleteBranch.vue
+++ src/components/TestDeleteBranch.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h1>marge code từ issue1-branch đến nhánh main</h1>
+        <ul>
+            <li>h1</li>
+            <li>h2</li>
+            <li>h3</li>
+        </ul>
     </div>
 </template>
 <script>
