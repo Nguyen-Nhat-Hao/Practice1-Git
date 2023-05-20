6fc4473141dbdc6b90a124059b8fe8dba2d2cfb6
Nguyen Nhat Hao <nguyennhathao20022018@gmail.com>
2023-05-20 13:33:39 +0700

    resolve-conflict-2 update: change header

--- src/components/TestDeleteBranch.vue
+++ src/components/TestDeleteBranch.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <h1>marge code từ issue1-branch đến nhánh main</h1>
-        <p>resolve conflict 2</p>
+        <div>
+            <h3>header 1</h3>
+            <p> i number one</p>
+
+        </div>
     </div>
 </template>
 <script>
